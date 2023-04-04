1f0e50a55bd7690cd7e46522fff803cfb831ef86
Alex Taranov <57453100+XelerT@users.noreply.github.com>
2023-04-05 00:14:36 +0300

    Update 4-3.py

--- 4-3.py
+++ 4-3.py
@@ -1,11 +1,15 @@
-import RPi.GPIO as gpio
+import RPi.GPIO as RG
 import sys
 from time import sleep
-gpio.setmode(gpio.BCM)
-gpio.setup(2, gpio.OUT)
-dac=[26, 19, 13, 6, 5, 11, 9, 10]                                                                                                                  
-gpio.setup(dac, gpio.OUT, initial=gpio.HIGH)
-pwm=gpio.PWM(2, 1000)                                    
+
+RG.setmode(RG.BCM)
+RG.setup(2, RG.OUT)
+
+dac=[26, 19, 13, 6, 5, 11, 9, 10]      
+
+RG.setup(dac, RG.OUT, initial=RG.HIGH)
+
+pwm=RG.PWM(2, 1000)                                    
 pwm.start(0)
 
 pwm
@@ -18,6 +22,6 @@ try:
                 pwm.ChangeDutyCycle(int(DutyCycle))
                 print("{:.2f}".format(int(DutyCycle)*3.3/100))
 finally:
-    gpio.output(2, 0)
-    gpio.output(dac, 0)
-    gpio.cleanup()   
+    RG.output(2, 0)
+    RG.output(dac, 0)
+    RG.cleanup()   
